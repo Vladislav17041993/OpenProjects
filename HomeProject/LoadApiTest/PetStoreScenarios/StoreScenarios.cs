3416e8cbbb3e90ec54ac5583fd9cf950ebe0756a
Владислав Дзятковский <v.a.dziatkovskii@monopoly.su>
2022-10-27 15:15:23 +0300

    delete get order scenario

--- HomeProject/LoadApiTest/PetStoreScenarios/StoreScenarios.cs
+++ HomeProject/LoadApiTest/PetStoreScenarios/StoreScenarios.cs
@@ -43,68 +43,7 @@ namespace LoadApiTest.PetStoreScenarios
             NBomberRunner
                 .RegisterScenarios(scenario)
                 .Run();
-        }
-
-        public void GetOrderLoadTest()
-        {
-            ConcurrentBag<long> orderIdArray = new(); 
-
-            var step = Step.Create("PostOrder_LoadTest", async context =>
-            {
-                var expected = StoreTestData.GenerateRandomOrderTestData();
-                var result = await petStore3RestSharp.PostOrder(expected);
-
-                var outputResponse = Response.Fail();
-
-                result.Switch(response =>
-                {
-                    if(response?.Content?.Id is not null)
-                        orderIdArray.Add((long)response.Content.Id);
-
-                    outputResponse = Response.Ok(statusCode: (int)response.HttpStatusCode);
-                },
-                error =>
-                {
-                    outputResponse = Response.Fail(error: error.Message, statusCode: (int)error.HttpStatusCode);
-                });
-
-                return outputResponse;
-            });
-
-            var dataFeed = Feed.CreateCircular("order id data", orderIdArray);
-
-            /*var step2 = Step.Create("GetOrder_LoadTest", feed: dataFeed,  async context =>
-            {
-                var result = await petStore3RestSharp.GetOrderById(feed);
-
-                var outputResponse = Response.Fail();
-
-                result.Switch(response =>
-                {
-                    if (response?.Content?.Id is not null)
-                        orderIdArray.Add((long)response.Content.Id);
-
-                    outputResponse = Response.Ok(statusCode: (int)response.HttpStatusCode);
-                },
-                error =>
-                {
-                    outputResponse = Response.Fail(error: error.Message, statusCode: (int)error.HttpStatusCode);
-                });
-
-                return outputResponse;
-            });*/
-
-            var scenario = ScenarioBuilder
-                .CreateScenario("PrepareData_PostOrders", step)
-                .WithLoadSimulations(
-                    Simulation.InjectPerSec(rate: 5, during: TimeSpan.FromSeconds(30))
-                );
-
-            NBomberRunner
-                .RegisterScenarios(scenario)
-                .Run();
-
-        }
+        }       
 
     }
 }
